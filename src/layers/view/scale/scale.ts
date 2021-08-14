260d8a943ace7bdadc3be37adfeb062e929147d2
Rizarid <rizarid@mail.ru>
2021-08-15 03:54:44 +0500

    Added Scale class

---
+++ src/layers/view/scale/scale.ts
@@ -0,0 +1,86 @@
+import { ChangeObserver } from '../../observers/change-observer';
+import { HorizontalCalculator } from '../orientation-calculator/horizontal-calculator';
+import { VerticalCalculator } from '../orientation-calculator/vertical-calculator';
+
+interface IScale {
+  scaleStep: number, extremeValues: number[], calculator: HorizontalCalculator | VerticalCalculator
+}
+interface ICallback { function: (eventObject: { eventName: string, eventBody }) => void }
+
+class Scale {
+  private body: HTMLElement;
+
+  private items: HTMLElement[];
+
+  private calculator: HorizontalCalculator | VerticalCalculator;
+
+  private changeObserver: ChangeObserver = new ChangeObserver();
+
+  constructor(options: IScale) {
+    this.calculator = options.calculator;
+    this.createScale();
+    this.createScaleItems(options.scaleStep, options.extremeValues);
+    this.items.map((item) => this.body.appendChild(item));
+    this.addListeners();
+  }
+
+  public subscribe(callback: ICallback): void {
+    this.changeObserver.subscribe(callback);
+  }
+
+  public unsubscribe(callback: ICallback): void {
+    this.changeObserver.unsubscribe(callback);
+  }
+
+  public getBody = (): HTMLElement => this.body;
+
+  private createScale(): void {
+    this.body = document.createElement('div');
+    this.body.className = 'light-range-slider__scale';
+  }
+
+  private getScaleItem = (value: number): HTMLElement => {
+    const item = document.createElement('div');
+    item.className = 'light-range-slider__scaleItem';
+    item.innerHTML = value.toString();
+    return item;
+  };
+
+  private createScaleItems = (scaleStep: number, extremeValues: number[]): void => {
+    this.items = [];
+
+    let i = extremeValues[0];
+    for (i; i < extremeValues[1]; i += scaleStep) {
+      this.items.push(this.getScaleItem(i));
+    }
+
+    this.items.push(this.getScaleItem(extremeValues[1]));
+
+    const marginRatio = this.calculator.getScaleMarginRatio(scaleStep);
+
+    this.items.map((item, index) => {
+      this.calculator.setElementsMargin(item, index * marginRatio);
+      return null;
+    });
+  };
+
+  private addListeners = (): void => {
+    this.items.map((item) => {
+      item.addEventListener('click', this.handleScaleItemClick);
+      return null;
+    });
+  };
+
+  private handleScaleItemClick = (event: PointerEvent): void => {
+    const newValue = Number(event.target.textContent!);
+
+    const eventObject = {
+      eventName: 'scaleItemClick',
+      eventBody: { newValue },
+    };
+
+    this.changeObserver.notify(eventObject);
+  };
+}
+
+export { Scale };
