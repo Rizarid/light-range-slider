d36fb37b6452a9e9dbdef26e74053798fed9c63e
Rizarid <rizarid@mail.ru>
2021-08-15 03:50:49 +0500

    Added Label class

---
+++ src/layers/view/label/label.ts
@@ -0,0 +1,35 @@
+import { HorizontalCalculator } from '../orientation-calculator/horizontal-calculator';
+import { VerticalCalculator } from '../orientation-calculator/vertical-calculator';
+
+interface IHandle {
+  margin: number,
+  value: number,
+  calculator: HorizontalCalculator | VerticalCalculator
+}
+
+class Label {
+  private body: HTMLElement;
+
+  private calculator: HorizontalCalculator | VerticalCalculator;
+
+  constructor(options: IHandle) {
+    this.calculator = options.calculator;
+    this.createLabel();
+    this.update(options.margin, options.value);
+  }
+
+  public getBody = (): HTMLElement => this.body;
+
+  public update = (margin: number, value: number): void => {
+    const correctMargin = this.calculator.getAdjustMarginToSize(this.body, margin);
+    this.calculator.setElementsMargin(this.body, correctMargin);
+    this.body.innerHTML = value.toString();
+  };
+
+  private createLabel(): void {
+    this.body = document.createElement('div');
+    this.body.className = 'light-range-slider__handle';
+  }
+}
+
+export { Label };
