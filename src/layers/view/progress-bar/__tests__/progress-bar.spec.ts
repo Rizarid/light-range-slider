007ee6878c23ef7fdccda29a2464f23c659b19ea
Rizarid <rizarid@mail.ru>
2021-08-24 15:39:28 +0500

    Cange tests of ProgressBar

--- src/layers/view/progress-bar/__tests__/progress-bar.spec.ts
+++ src/layers/view/progress-bar/__tests__/progress-bar.spec.ts
@@ -1,70 +1,45 @@
-import "chai"
+import { expect } from "chai"
 import { ProgressBar } from '../progress-bar'
 import { HorizontalCalculator } from '../../orientation-calculator/horizontal-calculator'
 import { VerticalCalculator } from '../../orientation-calculator/vertical-calculator'
 
 describe("ProgressBar", function(): void {
   let progressBar: ProgressBar
-  let calculator: HorizontalCalculator | VerticalCalculator;
+  let state = {
+    size: 0,
+    margin: 0
+  }
 
-  let body: HTMLElement = document.querySelector("body");
+  let calculator = { 
+    setElementsSize: (target: HTMLElement, size: number): void => { state.size = size },
+    setProgressBarMargin: (target: HTMLElement, margin: number): void => { state.margin = margin}
+  }
 
   beforeEach(function(): void {
-
-    let slider: HTMLElement = document.createElement("div");
-
-    body.innerHTML = `
-      <style> 
-        html{ margin: 0; padding: 0 }
-        body{ margin: 0; padding: 0 }
-        .slider{ width: 500px; height: 20px; margin: 20px 0 0 50px; padding: 0; position: relative }
-        .light-range-slider__progress-bar{ height: 100%; position: absolute }
-
-        .slider.slider_vertical{ width: 20px; height: 700px; position: relative}
-        .slider.slider_vertical .light-range-slider__progress-bar{ width: 100%; position: absolute }
-      </style>`
-
-    body.appendChild(slider);
-    slider.className = "slider";
-
-    let view = {
-        getLineSize: () => slider.offsetWidth,
-        getLineLocation: () => slider.getBoundingClientRect().left
-    }
-
-    calculator = new HorizontalCalculator({ 
-      getLineSize: view.getLineSize, 
-      getLineLocation: view.getLineLocation 
-    })
-
-    progressBar = new ProgressBar({ calculator })
-    slider.appendChild(progressBar.getBody());
-  })
-
-  afterEach( function (): void {
-    let body: HTMLElement = document.querySelector("body");
-    body.innerHTML = "";
+    state.size = 0;
+    state.margin = 0;
+    
+    progressBar = new ProgressBar({ calculator: (calculator as HorizontalCalculator) })
   })
 
   it('Should return div with class "light-range-slider__progress-bar"', function() {
-    expect(document.querySelector('.light-range-slider__progress-bar').tagName).to.equal('DIV');
+    expect(progressBar.getBody().tagName).to.equal('DIV');
+    expect(progressBar.getBody().className).to.equal('light-range-slider__progress-bar');
   })
 
-  it('Should update margin in case of not interval', function() {
-    progressBar.update([60]);
-
-    expect(progressBar.getBody().offsetLeft).to.equal(0);
-    expect(progressBar.getBody().offsetWidth).to.equal(300);
+  it('Should return size of progressBar', function() {
+    expect(progressBar.getProgressBarSize([200])).to.equal(200);
+    expect(progressBar.getProgressBarSize([200, 300])).to.equal(100);
   })
 
-  it('Should update margin in case of interval', function() {
-    const slider = document.querySelector('.slider')
-    slider.appendChild(progressBar.getBody());
-
-    progressBar.update([20, 60]);
-
-    expect(progressBar.getBody().offsetLeft).to.equal(100);
-    expect(progressBar.getBody().offsetWidth).to.equal(200);
+  it('Should return margin of progressBar', function() {
+    expect(progressBar.getProgressBarMargin([200])).to.equal(0);
+    expect(progressBar.getProgressBarMargin([200, 300])).to.equal(200);
   })
 
+  it('Should update size and margin of progressBar', function() {
+    progressBar.update([200, 300])
+    expect(state.size).to.equal(100);
+    expect(state.margin).to.equal(200);
+  })
 })
