756f2b4c4777ea8889e0a84ee04d3f54cd322bbd
Rizarid <rizarid@mail.ru>
2021-08-24 15:17:06 +0500

    Change tests of line

--- src/layers/view/line/__tests__/line.spec.ts
+++ src/layers/view/line/__tests__/line.spec.ts
@@ -1,41 +1,29 @@
-import "chai"
+import { expect } from "chai"
 import { Line } from '../line'
 import { HorizontalCalculator } from '../../orientation-calculator/horizontal-calculator'
-import { VerticalCalculator } from '../../orientation-calculator/vertical-calculator'
+import { ChangeObserver } from "../../../observers/change-observer"
+
 
 describe("Line", function(): void {
   let line: Line
-  let calculator: HorizontalCalculator | VerticalCalculator;
+  let state = {
+    lineEventObject: undefined
+  }
 
-  let view = {
-    getLineSize: () => line.getSize(),
-    getLineLocation: () => line.getLocation()
+  let calculator = { 
+    getElementsSize: (target: HTMLElement): number => 500,
+    getElementsLocation: (target: HTMLElement): number => 50,
+    getCursorLocation: (event: { pageX: number, pageY:number }): number => event.pageX - 50,
+    pxToPercentages: (newValue: number): number => newValue / 500 * 100
+  };
+
+  let changeObserver = {
+    notify: (event): void => state.lineEventObject = event,
   }
 
   beforeEach(function(): void {
-
-    let div: HTMLElement = document.createElement("div");
-    let body: HTMLElement = document.querySelector("body");
-
-    body.innerHTML = `
-      <style> 
-        body{ margin: 0; padding: 0 }
-        .slider{ width: 500px; height: 20px; margin: 20px 0 0 50px; padding: 0 }
-        .light-range-slider__line{ width: 100%; height: 6px; }
-
-        .slider.slider_vertical{ width: 20px; height: 700px}
-        .slider.slider_vertical .light-range-slider__line{ height: 100%; width: 6px}
-      </style>`
-
-    body.appendChild(div);
-    div.className = "slider";
-
-    calculator = new HorizontalCalculator({ 
-      getLineSize: view.getLineSize, 
-      getLineLocation: view.getLineLocation 
-    })
-
-    line = new Line(calculator)
+    state.lineEventObject = undefined;
+    line = new Line({ calculator: (calculator as HorizontalCalculator), changeObserver: (changeObserver as ChangeObserver) })
   })
 
   afterEach( function (): void {
@@ -44,93 +32,37 @@ describe("Line", function(): void {
   })
 
   it('Should return div with class "light-range-slider__line"', function() {
-    const slider = document.querySelector('.slider')
-    slider.appendChild(line.getBody());
-    expect(document.querySelector('.light-range-slider__line').tagName).to.equal('DIV');
+    expect(line.getBody().tagName).to.equal('DIV');
+    expect(line.getBody().className).to.equal('light-range-slider__line');
   })
 
   it('Should return size of line', function() {
-    const slider = document.querySelector('.slider');
-    slider.appendChild(line.getBody());
     expect(line.getSize()).to.equal(500);
   })
 
   it('Should return location of line', function() {
-    const slider = document.querySelector('.slider');
-    slider.appendChild(line.getBody());
-    expect(line.getLocation()).to.equal(50);
+    expect(Math.round(line.getLocation())).to.equal(50);
+  })
+
+  it('Should set parameter isResizeBlocked', function() {
+    expect(line.isResizeBlocked).to.be.true;
+    line.setIsResizeBlocked(false);
+    expect(line.isResizeBlocked).to.be.false;
   })
 
   it('Should send event object after click at line', function() {
-    
-    let testValue: {eventName: string, eventBody: { cursorLocation: number } }
-
-    const callback = (
-      event: {eventName: string, eventBody: { cursorLocation: number } }
-    ) => testValue = event
-    
-    const slider = document.querySelector('.slider');
-    slider.appendChild(line.getBody());
-
-    line.subscribe({ function: callback });
-    line.getBody().dispatchEvent(new Event('click')); 
-
-    expect(testValue.eventName).to.equal('lineClick');
-  })
-
-  it('Should create event object with horizontal value', function() {
-    const eventObject = {
-      eventName: 'lineClick',
-      eventBody: { newValue: 40 }
-    }
-
-    let testValue: {eventName: string, eventBody: { cursorLocation: number } }
-
-    const callback = (
-      event: {eventName: string, eventBody: { cursorLocation: number } }
-    ) => testValue = event
-    
-    const slider = document.querySelector('.slider');
-    slider.appendChild(line.getBody());
-
-    line.subscribe({ function: callback });
-    line.handleLineClick({ pageX: 250, pageY: 200 })
-
-    expect(testValue).to.deep.equal(eventObject);
-  })
-
-  it('Should create event object with vertical value', function() {
-    const eventObject = {
-      eventName: 'lineClick',
-      eventBody: { newValue: 74.28571428571429 }
-    }
-
-    calculator = new VerticalCalculator({ 
-      getLineSize: view.getLineSize, 
-      getLineLocation: view.getLineLocation 
-    })
-
-    line = new Line(calculator)
-
-    let testValue: {eventName: string, eventBody: { cursorLocation: number } }
-
-    const callback = (
-      event: {eventName: string, eventBody: { cursorLocation: number } }
-    ) => testValue = event
-    
-    const slider = document.querySelector('.slider');
-    slider.appendChild(line.getBody());
-    slider.classList.add('slider_vertical');
-
-    line.subscribe({ function: callback });
-    line.handleLineClick({ pageX: 250, pageY: 200 })
-
-    expect(testValue).to.deep.equal(eventObject);
-  })
-
   
+    const body = document.querySelector('body');
+    body.appendChild(line.getBody());
 
+    line.getBody().dispatchEvent(new MouseEvent('click', { clientX: 300 })); 
 
+    const eventObject = {
+      eventName: 'lineClick',
+      eventBody: { newValue: 50 },
+    }; 
 
+    expect(state.lineEventObject).to.deep.equal(eventObject);
+  })
 
 })
