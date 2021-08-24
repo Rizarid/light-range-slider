b645f16f4f4da299b948fdee6e2d820212c40eb8
Rizarid <rizarid@mail.ru>
2021-08-24 13:28:41 +0500

    Changed labels tests

--- src/layers/view/label/__tests__/label.spec.ts
+++ src/layers/view/label/__tests__/label.spec.ts
@@ -1,64 +1,42 @@
-import "chai"
+import { expect } from "chai"
+import { HorizontalCalculator } from "../../orientation-calculator/horizontal-calculator"
 import { Label } from '../label'
-import { HorizontalCalculator } from '../../orientation-calculator/horizontal-calculator'
-import { VerticalCalculator } from '../../orientation-calculator/vertical-calculator'
+
 
 describe("Label", function(): void {
   let label: Label
-  let calculator: HorizontalCalculator | VerticalCalculator;
+  let state = { margin: 0 }
 
-  let body: HTMLElement = document.querySelector("body");
-
-  beforeEach(function(): void {
-
-    let slider: HTMLElement = document.createElement("div");
-
-    body.innerHTML = `
-      <style> 
-        body{ margin: 0; padding: 0 }
-        .slider{ width: 500px; height: 20px; margin: 20px 0 0 50px; padding: 0; position: relative }
-        .light-range-slider__label{ width: fit-content; height: fit-content; position: absolute }
-
-        .slider.slider_vertical{ width: 20px; height: 700px; position: relative}
-        .slider.slider_vertical .light-range-slider__label{ width: fit-content; height: fit-content; position: absolute }
-      </style>`
-
-    body.appendChild(slider);
-    slider.className = "slider";
-
-    let view = {
-      getLineSize: () => slider.offsetWidth,
-      getLineLocation: () => slider.getBoundingClientRect().left
+  let calculator = {
+    getAdjustedMarginToSize: (target: HTMLElement, marginInPercent: number): number => {
+      return marginInPercent - 5.1;
+    },
+    setElementsMargin: (target: HTMLElement, newValueInPercent: number): void => {
+      state.margin = newValueInPercent;
     }
-
-    calculator = new HorizontalCalculator({ 
-      getLineSize: view.getLineSize, 
-      getLineLocation: view.getLineLocation 
-    })
-
-    label = new Label({ value: 50, calculator })
-    slider.appendChild(label.getBody());
-    label.update({ margin: 50, value: 50, collection: []})
-  })
-
-  afterEach( function (): void {
-    let body: HTMLElement = document.querySelector("body");
-    body.innerHTML = "";
+  }
+  
+  beforeEach(function(): void {
+    state.margin = 0;
+    label = new Label({ calculator: (calculator as HorizontalCalculator), isCollection: false})
   })
 
   it('Should return div with class "light-range-slider__label"', function() {
-    expect(document.querySelector('.light-range-slider__label').tagName).to.equal('DIV');
+    expect(label.getBody().tagName).to.equal('DIV');
+    expect(label.getBody().className).to.equal('light-range-slider__label');
   })
 
-  it('Should update margin', function() {
-
-    expect(label.getBody().offsetLeft).to.equal(242);
-    expect(label.getBody().innerText).to.equal('50');
-
+  it('Should update label by isCollection = false', function() {
     label.update({ margin: 60, value: 60, collection: []});
-
-    expect(label.getBody().offsetLeft).to.equal(292);
+    expect(state.margin).to.equal(55);
     expect(label.getBody().innerText).to.equal('60');
+  })
+
+  it('Should update label by isCollection = true', function() {
+    label = new Label({ calculator: (calculator as HorizontalCalculator), isCollection: true})
+    label.update({ margin: 50, value: 1, collection: ['one', 'two', 'three']});
+    expect(state.margin).to.equal(45);
+    expect(label.getBody().innerText).to.equal('two');
   })
 
 })
