5bfd6f26c0eb44bfff921dfe841c0f1657b89691
Rizarid <rizarid@mail.ru>
2021-09-21 13:48:15 +0500

    Refactoring of demo page

--- src/demo/demo.ts
+++ src/demo/demo.ts
@@ -1,20 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { SlidersDemo } from './components/sliders-demo/sliders-demo';
-import './demo.sass';
-import '../lite-range-slider/lite-range-slider';
 
-const demos = document.querySelectorAll('.js-sliders-demo');
-const firstDemo = new SlidersDemo((demos[0] as HTMLElement));
-const secondDemo = new SlidersDemo((demos[1] as HTMLElement), {
-  extremeValues: [-5, 5],
-  step: 0.1,
-  scaleStep: 1,
-  isInterval: true,
-  currentValues: [-2, 2],
-  isVertical: true,
-});
-const thirdDemo = new SlidersDemo((demos[2] as HTMLElement), {
-  collection: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'],
-  isCollection: true,
-  currentValues: [6],
-});
+import '../lite-range-slider/lite-range-slider';
+import './components/sliders-demo/sliders-demo-init';
+import './demo.sass';
