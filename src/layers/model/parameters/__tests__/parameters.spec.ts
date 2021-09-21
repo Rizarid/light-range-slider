d46851860faf132528d42f3ab9f3f6e50c532d57
Rizarid <rizarid@mail.ru>
2021-09-22 00:32:29 +0500

    Added class Parameters to model

---
+++ src/layers/model/parameters/__tests__/parameters.spec.ts
@@ -0,0 +1,184 @@
+import { expect } from "chai";
+import { ChangeObserver } from "../../../observers/change-observer";
+import { Parameters } from "../parameters";
+
+describe("Parameters", function (): void{
+
+  let parameters: Parameters;
+
+  beforeEach( function () :void{
+    parameters = new Parameters({
+      extremeValues: [300, 400],
+      currentValues: [330, 370],
+      step: 1,
+      scaleStep: 10,
+      isVertical: false,
+      isInterval: true,
+      haveProgressBar: true,
+      haveScale: true,
+      haveLabel: true,
+      callbacks: [],
+      isCollection: false,
+      collection: [],
+      changeObserver: new ChangeObserver()
+    });
+
+  })
+
+  it("Should init parameters" , function() {
+    expect(parameters.getExtremeValues()).to.deep.equal([300, 400]);
+    expect(parameters.getCurrentValues()).to.deep.equal([330, 370]);
+    expect(parameters.getStep()).to.equal(1);
+    expect(parameters.getScaleStep()).to.equal(10);
+    expect(parameters.getIsVertical()).to.be.false;
+    expect(parameters.getIsInterval()).to.be.true;
+    expect(parameters.getHaveLabel()).to.be.true;
+    expect(parameters.getHaveScale()).to.be.true;
+  })
+
+  it("Should return and set parameter extremeValues" , function() {
+    expect(parameters.getExtremeValues()).to.deep.equal([300, 400]);
+    parameters.setExtremeValues([200, 400])
+    expect(parameters.getExtremeValues()).to.deep.equal([200, 400]);
+  })
+
+  it("Should change parameter currentValues if when changing parameter extremeValues currentValues went out of range" , function() {
+    expect(parameters.getExtremeValues()).to.deep.equal([300, 400]);
+    expect(parameters.getCurrentValues()).to.deep.equal([330, 370]);
+
+    parameters.setExtremeValues([200, 300]);
+
+    expect(parameters.getExtremeValues()).to.deep.equal([200, 300]);
+    expect(parameters.getCurrentValues()).to.deep.equal([300, 300]);
+  })
+
+  it("Should return and set parameter currentValues" , function() {
+    expect(parameters.getCurrentValues()).to.deep.equal([330, 370]);
+    parameters.setCurrentValues([310, 360])
+    expect(parameters.getCurrentValues()).to.deep.equal([310, 360]);
+  })
+
+  it("Should return and set parameter step" , function() {
+    expect(parameters.getStep()).to.equal(1);
+    parameters.setStep(5)
+    expect(parameters.getStep()).to.equal(5);
+  })
+
+  it("Should return and set parameter scaleStep" , function() {
+    expect(parameters.getScaleStep()).to.equal(10);
+    parameters.setScaleStep(15)
+    expect(parameters.getScaleStep()).to.equal(15);
+  })
+
+  it("Should return and set parameter isVertical" , function() {
+    expect(parameters.getIsVertical()).to.be.false;
+    parameters.setIsVertical(true)
+    expect(parameters.getIsVertical()).to.be.true;
+  })
+
+  it("Should return and set parameter isInterval" , function() {
+    expect(parameters.getIsInterval()).to.be.true;
+    parameters.setIsInterval(false)
+    expect(parameters.getIsInterval()).to.be.false;
+  })
+
+  it("Should correct quantity currentValues after change parameter isInterval" , function() {
+    expect(parameters.getIsInterval()).to.be.true;
+    expect(parameters.getCurrentValues().length).to.equal(2)
+
+    parameters.setIsInterval(false);
+
+    expect(parameters.getIsInterval()).to.be.false;
+    expect(parameters.getCurrentValues().length).to.equal(1)
+
+    parameters.setIsInterval(true);
+
+    expect(parameters.getIsInterval()).to.be.true;
+    expect(parameters.getCurrentValues().length).to.equal(2)
+  })
+
+  it("Should return and set parameter haveLabel" , function() {
+    expect(parameters.getHaveLabel()).to.be.true;
+    parameters.setHaveLabel(false)
+    expect(parameters.getHaveLabel()).to.be.false;
+  })
+
+  it("Should return and set parameter haveScale" , function() {
+    expect(parameters.getHaveScale()).to.be.true;
+    parameters.setHaveScale(false)
+    expect(parameters.getHaveScale()).to.be.false;
+  })
+
+  it("Should return and set parameter haveProgressBar" , function() {
+    expect(parameters.getHaveProgressBar()).to.be.true;
+    parameters.setHaveProgressBar(false)
+    expect(parameters.getHaveProgressBar()).to.be.false;
+  })
+
+  it("Should return and set parameter collection" , function() {
+    expect(parameters.getCollection()).to.deep.equal([]);
+    parameters.setCollection(['one', 'two', 'three']);
+    expect(parameters.getCollection()).to.deep.equal(['one', 'two', 'three']);
+  })
+
+  it("Should return and set parameter isCollection" , function() {
+    expect(parameters.getIsCollection()).to.be.false;
+    parameters.setCollection(['one', 'two', 'three'])
+    parameters.setIsCollection(true)
+    expect(parameters.getIsCollection()).to.be.true;
+    expect(parameters.getStep()).to.equal(1);
+    expect(parameters.getScaleStep()).to.equal(1);
+    expect(parameters.getCurrentValues()).to.deep.equal([2, 2]);
+    expect(parameters.getExtremeValues()).to.deep.equal([0, 2]);
+  })
+
+  it("Should send updates after change parameters" , function() {
+    const valuesUpdate = {
+      eventName: 'valuesUpdate',
+      eventBody: {
+        extremeValues: [300, 400],
+        currentValues: [320, 370],
+        margins: [20, 70],
+        step: 1,
+        scaleStep: 10,
+        isVertical: false,
+        isInterval: true,
+        haveProgressBar: true,             
+        haveScale: true,     
+        haveLabel: true,
+        isCollection: false,
+        collection: []
+      }
+    }
+
+    const fullUpdate = {
+      eventName: 'fullUpdate',
+      eventBody: {
+        extremeValues: [300, 400],
+        currentValues: [320],
+        margins: [20],
+        step: 1,
+        scaleStep: 10,
+        isVertical: false,
+        isInterval: false,
+        haveProgressBar: true,             
+        haveScale: true,     
+        haveLabel: true,
+        isCollection: false,
+        collection: []
+      }
+    }
+
+    let testValue: { eventName: string, eventBody };
+    const callback = (event: { eventName: string, eventBody }):void => { testValue = event };
+    parameters.subscribe({ function: callback });
+
+    parameters.setCurrentValues([320, 370]);
+
+    expect(testValue).to.deep.equal(valuesUpdate);
+
+    parameters.setIsInterval(false);
+
+    expect(testValue).to.deep.equal(fullUpdate);
+  })
+})
---
+++ src/layers/model/parameters/parameters.ts
@@ -0,0 +1,268 @@
+import {
+  IParameters, ICallback, IUpdateBody, IUpdate,
+} from '../../interfaces/interfaces';
+import { ChangeObserver } from '../../observers/change-observer';
+import { ValueChecker } from '../value-checker/value-checker';
+
+class Parameters {
+  private extremeValues: number[];
+
+  private currentValues: number[];
+
+  private callbacks: ((updateObject: IUpdateBody) => void)[] = [];
+
+  private collection: string[] | number[] | HTMLElement[] = [];
+
+  private step: number;
+
+  private scaleStep: number;
+
+  private isVertical: boolean;
+
+  private isInterval: boolean;
+
+  private haveProgressBar: boolean;
+
+  private haveScale: boolean;
+
+  private haveLabel: boolean;
+
+  private isCollection: boolean;
+
+  private changeObserver: ChangeObserver;
+
+  private isInit = true;
+
+  private valueChecker: ValueChecker = new ValueChecker();
+
+  constructor(options: IParameters) {
+    const {
+      extremeValues, currentValues, step, scaleStep, isVertical, isInterval, haveProgressBar,
+      haveLabel, haveScale, callbacks, collection, isCollection, changeObserver,
+    } = options;
+
+    this.changeObserver = changeObserver;
+
+    this.setExtremeValues(extremeValues);
+    this.setCurrentValues(currentValues);
+    this.setStep(step);
+    this.setScaleStep(scaleStep);
+    this.setIsVertical(isVertical);
+    this.setIsInterval(isInterval);
+    this.setHaveProgressBar(haveProgressBar);
+    this.setHaveLabel(haveLabel);
+    this.setHaveScale(haveScale);
+    this.setCallbacks(callbacks);
+    this.setCollection(collection);
+    this.setIsCollection(isCollection);
+
+    this.correctCurrentValueToInterval();
+    this.adjustQuantityOfCurrentValues();
+
+    this.isInit = false;
+  }
+
+  public subscribe(callback: ICallback): void {
+    this.changeObserver.subscribe(callback);
+  }
+
+  public unsubscribe(callback: ICallback): void {
+    this.changeObserver.subscribe(callback);
+  }
+
+  public getExtremeValues = (): number[] => {
+    const [min, max] = this.extremeValues;
+    return [min, max];
+  };
+
+  public setExtremeValues = (newValue: number[]): void => {
+    if (this.valueChecker.checkExtremeValues(newValue)) {
+      this.extremeValues = newValue;
+
+      if (!this.isInit) {
+        this.correctCurrentValueToInterval();
+        this.sendUpdate();
+      }
+    }
+  };
+
+  public getCurrentValues = (): number[] => {
+    const [min, max] = this.currentValues;
+    return this.isInterval ? [min, max] : [min];
+  };
+
+  public setCurrentValues = (newValue: number[]): void => {
+    if (this.valueChecker.checkCurrentValues(newValue)) {
+      this.currentValues = newValue;
+
+      if (!this.isInit) {
+        this.correctCurrentValueToInterval();
+        this.adjustQuantityOfCurrentValues();
+        this.sendUpdate('valuesUpdate');
+      }
+    }
+  };
+
+  public getStep = (): number => this.step;
+
+  public setStep = (newValue: number): void => {
+    if (this.valueChecker.checkStepAndScaleStep(newValue, this.extremeValues, this.isCollection)) {
+      this.step = newValue;
+      this.sendUpdate();
+    }
+  };
+
+  public getScaleStep = (): number => this.scaleStep;
+
+  public setScaleStep = (newValue: number): void => {
+    if (this.valueChecker.checkStepAndScaleStep(newValue, this.extremeValues, this.isCollection)) {
+      this.scaleStep = newValue;
+      this.sendUpdate();
+    }
+  };
+
+  public getIsVertical = (): boolean => this.isVertical;
+
+  public setIsVertical = (newValue: boolean): void => {
+    if (this.valueChecker.checkBoolean(newValue)) {
+      this.isVertical = newValue;
+      this.sendUpdate();
+    }
+  };
+
+  public getIsInterval = (): boolean => this.isInterval;
+
+  public setIsInterval = (newValue: boolean): void => {
+    if (this.valueChecker.checkBoolean(newValue)) {
+      this.isInterval = newValue;
+      this.adjustQuantityOfCurrentValues();
+      this.sendUpdate();
+    }
+  };
+
+  public getHaveProgressBar = (): boolean => this.haveProgressBar;
+
+  public setHaveProgressBar = (newValue: boolean): void => {
+    if (this.valueChecker.checkBoolean(newValue)) {
+      this.haveProgressBar = newValue;
+      this.adjustQuantityOfCurrentValues();
+      this.sendUpdate();
+    }
+  };
+
+  public getHaveLabel = (): boolean => this.haveLabel;
+
+  public setHaveLabel = (newValue: boolean): void => {
+    if (this.valueChecker.checkBoolean(newValue)) {
+      this.haveLabel = newValue;
+      this.sendUpdate();
+    }
+  };
+
+  public getHaveScale = (): boolean => this.haveScale;
+
+  public setHaveScale = (newValue: boolean): void => {
+    if (this.valueChecker.checkBoolean(newValue)) {
+      this.haveScale = newValue;
+      this.sendUpdate();
+    }
+  };
+
+  public getCallbacks = (): ((updateObject: IUpdateBody) => void)[] => this.callbacks;
+
+  public setCallbacks = (newValue: ((updateObject: IUpdateBody) => void)[]): void => {
+    if (this.valueChecker.checkCallbacks(newValue)) this.callbacks = newValue;
+  };
+
+  public getCollection = (): string[] | number[] | HTMLElement[] => this.collection;
+
+  public setCollection = (newValue: string[] | number[] | HTMLElement[]): void => {
+    if (this.valueChecker.checkCollection(newValue)) {
+      this.collection = newValue;
+      if (this.isCollection) this.setExtremeValues([0, this.collection.length - 1]);
+      this.sendUpdate();
+    }
+  };
+
+  public getIsCollection = (): boolean => this.isCollection;
+
+  public setIsCollection = (newValue: boolean): void => {
+    if (this.valueChecker.checkIsCollection(newValue, this.collection)) {
+      if (!newValue) {
+        this.isCollection = newValue;
+        this.sendUpdate();
+      } else {
+        this.step = 1;
+        this.scaleStep = 1;
+        this.currentValues = this.currentValues.map((item) => Math.round(item));
+        this.isCollection = newValue;
+        this.setExtremeValues([0, this.collection.length - 1]);
+      }
+    }
+  };
+
+  public sendUpdate = (eventName?: string): void => {
+    let eventObject: IUpdate;
+
+    if (eventName === 'valuesUpdate') eventObject = this.getUpdate('valuesUpdate');
+    else if (eventName === 'scaleUpdate') eventObject = this.getUpdate('scaleUpdate');
+    else eventObject = this.getUpdate('fullUpdate');
+
+    this.notifyCallbacks(eventObject.eventBody);
+    this.changeObserver.notify(eventObject);
+  };
+
+  private correctCurrentValueToInterval = (): void => {
+    const [minValue, maxValue] = this.extremeValues;
+    this.currentValues = this.currentValues.map((item) => ((item < minValue) ? minValue : item));
+    this.currentValues = this.currentValues.map((item) => ((item > maxValue) ? maxValue : item));
+  };
+
+  private adjustQuantityOfCurrentValues(): void {
+    const [minCurrentValue] = this.currentValues;
+    const maxValue = this.extremeValues[1];
+
+    if (this.isInterval && (this.currentValues.length === 1)) {
+      this.currentValues = [minCurrentValue, maxValue];
+    }
+
+    if (!this.isInterval && (this.currentValues.length === 2)) {
+      this.currentValues = [minCurrentValue];
+    }
+  }
+
+  public getUpdate = (eventName: string): IUpdate => ({
+    eventName,
+    eventBody: {
+      extremeValues: this.extremeValues,
+      currentValues: this.currentValues,
+      margins: this.currentValues.map((item) => this.valueToPercent(item)),
+      step: this.step,
+      scaleStep: this.scaleStep,
+      isVertical: this.isVertical,
+      isInterval: this.isInterval,
+      haveProgressBar: this.haveProgressBar,
+      haveScale: this.haveScale,
+      haveLabel: this.haveLabel,
+      isCollection: this.isCollection,
+      collection: this.collection,
+    },
+  });
+
+  private notifyCallbacks = (updateObject: IUpdateBody): void => {
+    try {
+      if (this.callbacks.length) this.callbacks.map((item) => item(updateObject));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  private valueToPercent = (value: number): number => {
+    const [min, max] = this.extremeValues;
+    const range = max - min;
+    const valueInRange = value - min;
+    return (valueInRange / range) * 100;
+  };
+}
+
+export { Parameters };
