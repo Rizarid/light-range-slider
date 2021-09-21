1cbe386f608425b51c113b0f3b50bdd799f73f68
Rizarid <rizarid@mail.ru>
2021-09-22 00:25:21 +0500

    Moved calculator from controller to model

---
+++ src/layers/model/calculator/calculator.ts
@@ -0,0 +1,65 @@
+import { Parameters } from '../parameters/parameters';
+
+class Calculator {
+  private parameters: Parameters;
+
+  constructor(parameters: Parameters) {
+    this.parameters = parameters;
+  }
+
+  public valueToPercent = (value: number): number => {
+    const range = this.getRange();
+    const valueInRange = this.valueToValueInRange(value);
+    return (valueInRange / range) * 100;
+  };
+
+  public percentToValue = (percentValue: number): number => {
+    const valueInRange = this.percentToValueInRange(percentValue);
+    return this.valueInRangeToValue(valueInRange);
+  };
+
+  public adjustByStep = (value: number): number => {
+    const step = this.parameters.getStep();
+    let valueInRange = this.valueToValueInRange(value);
+    if (this.checkForExceedingTheLastStep(valueInRange)) return this.getRange();
+    valueInRange = Math.round(valueInRange / step) * step;
+    return this.valueInRangeToValue(valueInRange);
+  };
+
+  public adjustByAccuracy = (value: number): number => {
+    const accuracy = 10 ** this.getNumberOfDecimalPlaces(this.parameters.getStep());
+    return Math.round(value * accuracy) / accuracy;
+  };
+
+  private percentToValueInRange = (percentValue: number): number => (
+    this.getRange() * (percentValue / 100)
+  );
+
+  private getNumberOfDecimalPlaces = (value: number): number => {
+    const str = value.toString();
+    return str.includes('.', 0) ? str.split('.').pop().length : 0;
+  };
+
+  private checkForExceedingTheLastStep = (valueInRange: number): boolean => {
+    const range = this.getRange();
+    const step = this.parameters.getStep();
+    return (Math.round((valueInRange / step) * 10) / 10 > Math.round(range / step));
+  };
+
+  private getRange = (): number => {
+    const [minValue, maxValue] = this.parameters.getExtremeValues();
+    return maxValue - minValue;
+  };
+
+  private valueToValueInRange = (value: number): number => {
+    const [minValue] = this.parameters.getExtremeValues();
+    return value - minValue;
+  };
+
+  private valueInRangeToValue = (value: number): number => {
+    const [minValue] = this.parameters.getExtremeValues();
+    return value + minValue;
+  };
+}
+
+export { Calculator };
